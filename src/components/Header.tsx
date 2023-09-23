12fd5c130c9e05cc3853f9f02ac0c7243dea6627
yijin0907 <yijin0907@gmail.com>
2023-09-23 19:34:43 +0800

    Change images path and add file src alias.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,65 @@
-import Container from './Container'
-import logImg from '../assets/logo.svg'
-
 const Header: React.FC = () => {
     return (
         <>
-            <Container>
-                <div className="bg-primary p-4">
+            <div className="bg-warning text-base-content px-32 py-3 grid grid-cols-2 gap-4">
+                <div>
                     <div className="avatar">
-                        <div className="w-12 rounded-full ring-1 ring-offset-2 ring-blue-500/[.55]">
-                            <img src={logImg} />
+                        <div className="w-16 mask mask-squircle">
+                            <img src="/images/logo.svg" />
                         </div>
                     </div>
                 </div>
-            </Container>
+                <div className="flex flex-row-reverse">
+                    <div className="p-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                            />
+                        </svg>
+                    </div>
+                    <div className="p-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 15.75V18m-7.5-6.75h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V13.5zm0 2.25h.008v.008H8.25v-.008zm0 2.25h.008v.008H8.25V18zm2.498-6.75h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V13.5zm0 2.25h.007v.008h-.007v-.008zm0 2.25h.007v.008h-.007V18zm2.504-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zm0 2.25h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V18zm2.498-6.75h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V13.5zM8.25 6h7.5v2.25h-7.5V6zM12 2.25c-1.892 0-3.758.11-5.593.322C5.307 2.7 4.5 3.65 4.5 4.757V19.5a2.25 2.25 0 002.25 2.25h10.5a2.25 2.25 0 002.25-2.25V4.757c0-1.108-.806-2.057-1.907-2.185A48.507 48.507 0 0012 2.25z"
+                            />
+                        </svg>
+                    </div>
+                    <div className="p-4">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,30 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Container from '@/components/Container'
+import bgImg from '/images/dashboard.jpg'
 
 const Home: React.FC = () => {
     return (
         <>
             <Header />
-            <div>
-                <button className="btn btn-secondary">click</button>
-            </div>
+            <div className="divider"></div>
+            <Container>
+                <div className="hero min-h-screen bg-cover" style={{ backgroundImage: `url(${bgImg})` }}>
+                    <div className="hero-overlay bg-opacity-80"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="max-w-4xl">
+                            <h1 className="mb-5 text-5xl font-bold">Medical Underwriting</h1>
+                            <p className="mb-5">
+                                A process used by insurance companies to try to figure out your health status when
+                                you're applying for health insurance coverage to determine whether to offer you
+                                coverage, at what price, and with what exclusions or limits.
+                            </p>
+                            <button className="btn btn-primary">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+            <div className="divider"></div>
             <Footer />
         </>
     )
